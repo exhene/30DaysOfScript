e447740e9835b2fc93493976b76c1249be79ca79
hi <87506323+exhene@users.noreply.github.com>
2023-02-21 23:04:16 +0000

    Old files
    Some older files that may or may not be incomplete. Taken from the 30DaysofJS course on Github.

---
+++ 30DaysOfScript/Day01/Day01ex/variable.js
@@ -0,0 +1,15 @@
+let dumbNigga = 'Jack2'       // this nigga dumb
+let bitchNigga = 'yopopopo'       // this nigga a bitch
+
+'yopopopo'
+"yopopopo"
+`yopopopo`
+'Jack2'
+"Jack2"
+`Jack2`
+
+
+let Jack2
+console.log(Jack2)
+
+let emptyvalue = null
